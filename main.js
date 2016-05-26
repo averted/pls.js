61b29fafd9f9c0879037035c406ee4362feafb94
averted <vitiuk.alex@gmail.com>
2016-05-26 10:51:23 -0400

    Finalizing main, needs better output

--- main.js
+++ main.js
@@ -1,37 +1,66 @@
-var globby = require('globby')
-  , exec = require('child_process').execSync;
+;(function() {
+  var exec = require('child_process').execSync
+    , globby = require('globby');
 
-var blacklist = {
-  extention: [
-  'json',
-  ],
+  var BLACKLIST = ['node_modules'];
 
-  directory: [
-    'node_modules',
-  ],
+  var options = {
+    // glob options
+    get: function() {
+      return {
+        nodir: true,
+        ignore: this.parse(process.argv.slice(2, process.argv.length)),
+      };
+    },
 
-  string: function() {
+    // parse process args
+    parse: function(args) {
+      var blacklist = BLACKLIST;
 
-  }
-};
+      if (args.length) {
+        args.map(function(arg) {
+          var parts = arg.split('=')
+            , param = parts[0]
+            , value = parts[1];
 
-function readFiles(files) {
-  var code = {};
+          if (param === '-ignore' || param === '-i' || param === '-exclude') {
+            blacklist.push(this.trim(value));
+          }
+        }.bind(this));
+      }
 
-  files.forEach(function(path) {
-    var lines = exec('cat ' + path + ' | wc -l', { encoding: 'utf8' })
-      , parts = path.split('.')
-      , ext   = parts[parts.length - 1];
+      blacklist.map(function(item, index) {
+        this[index] += '/**';
+      }.bind(blacklist));
 
-    code[ext] = code[ext] || 0;
-    code[ext] += parseInt(lines);
+      return blacklist;
+    },
+
+    trim: function(value) {
+      if (value.slice(-1) === '/') {
+        value = value.substring(0, value.length - 1);
+      }
+
+      return value;
+    }
+  };
+
+  function readFiles(files) {
+    var code = {};
+
+    files.forEach(function(path) {
+      var lines = exec('cat ' + path + ' | wc -l', { encoding: 'utf8' })
+        , parts = path.split('.')
+        , ext   = parts[parts.length - 1];
+
+      code[ext] = code[ext] || 0;
+      code[ext] += parseInt(lines);
+    });
+
+    return code;
+  };
+
+  globby(['**/*'], options.get()).then(function(files) {
+    console.dir(readFiles(files));
   });
-
-  return code;
-};
-
-console.log('arguments', process.argv);
-
-globby(['**/*'], { nodir: true, ignore: ['node_modules/**'] }).then(function(files) {
-  console.dir(readFiles(files));
-});
+})();
